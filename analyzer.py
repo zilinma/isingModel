0dbdecf7349116a93ffcd8b3a1323757a57a67d6
zm003 <zm003@bucknell.edu>
2018-03-08 22:34:01 -0500

    ising models

--- analyzer.py
+++ analyzer.py
@@ -10,10 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 SIZE = 100
-STEPS = 20000
+STEPS = 1500
 START = 200
 END = 1000
-T = 2.25
+T = 2.48
+
+
 PATH = './data'
 magnetization = np.zeros(STEPS)
 energy = np.zeros(STEPS)
@@ -37,13 +39,13 @@ for i in range(STEPS - END):
 
 f, ax1 = plt.subplots(1, 2, figsize=(7, 3))
 
-ax1[0].plot(co)
+ax1[0].plot(energy)
 ax1[1].plot(magnetization)
 plt.axhline()
 plt.show()
 
-print(np.mean(magnetization[2000:]))
-print(np.mean(energy[2000:]))
-print(np.var(magnetization[2000:]))
+print(np.mean(magnetization[200:]))
+print(np.mean(energy[200:]))
+print(np.var(magnetization[200:]))
 
-print(np.var(energy[2000:]))
+print(np.var(energy[200:]))
--- plotter.py
+++ plotter.py
@@ -11,14 +11,21 @@ def readTot(filePath):
     magnetization = []
     temperature = []
     with open(filePath) as csvfile:
-        testReader = csv.reader(csvfile, delimiter=' ',
+        testReader = csv.reader(csvfile, delimiter=',',
                                 quotechar='|')
+        next(testReader, None)
         for row in testReader:
-            temperature.append(row[0])
-            magnetization.append(row[1])
-            energy.append([2])
+            temperature.append(float(row[0]))
+            magnetization.append(float(row[1]))
+            energy.append(float(row[2]))
 
     return energy, magnetization, temperature
+
+
+def theoryMag(x):
+
+    return np.power(1-np.power(np.sinh(2/x),-4),1/8)
+
 
 def plotEMDEDT(energy, magnetization, temperature):
     '''
@@ -27,17 +34,26 @@ def plotEMDEDT(energy, magnetization, temperature):
     :param magnetization: an array that contains magnetization values for different T.
     :param temperature: an array that contains all of the temperature
     '''
-    derivative = (np.roll(energy, 1) - energy)/ (np.roll(temperature, 1) - temperature)
-    f, ax = plt.subplots(3, figsize=(7, 3))
+    derivative = (np.roll(energy[::5], 1) - energy[::5])/ (np.roll(temperature[::5], 1) - temperature[::5])
+    f, ax = plt.subplots(1, 3, figsize=(7, 3))
 
-    ax[0].plot(temperature, energy)
-    ax[0].axhline()
+    ax[0].plot(temperature, energy, 'ro')
+    #ax[0].axhline()
     ax[0].set_title("E")
-    ax[1].plot(temperature, magnetization)
-    ax[1].axhline()
+
+    ax[1].plot(temperature, magnetization, 'ro')
+    x = np.linspace(1.9, 2.268, 1000)
+    ax[1].plot(np.append(x, 2.269),
+                  np.append(theoryMag(x), 0))
+    #ax[0][1].axhline()
     ax[1].set_title("M")
 
-    ax[2].plot(temperature, derivative)
-    ax[2].axhline()
+
+    ax[2].plot(temperature[::5], derivative)
+    #ax[2].axhline()
     ax[2].set_title(r'$\frac{dE}{dT}$')
     plt.show()
+
+
+energy, magnetization, temperature = readTot("isingTot.csv")
+plotEMDEDT(energy, magnetization, temperature)
